381b2755fe0607e7ffb3cf1cb6bc14f8324214ea
FRBF Studios <tfroempp@outlook.de>
2026-02-23 12:29:29 +0100

    Added base.h as a single-include API

---
+++ include/caffeine-gl/base.h
@@ -0,0 +1,13 @@
+#ifndef BASE_H
+#define BASE_H
+
+#include <caffeine-gl/game/Game.h>
+#include <caffeine-gl/game/GameObject.h>
+
+#include <caffeine-gl/gfx/Texture.h>
+#include <caffeine-gl/gfx/Shader.h>
+#include <caffeine-gl/gfx/ParticleGenerator.h>
+#include <caffeine-gl/gfx/ResourceManager.h>
+#include <caffeine-gl/gfx/SpriteRenderer.h>
+
+#endif //BASE_H
