d80f558826807692eb65da0a2ba57501f2a540e4
FRBF Studios <tfroempp@outlook.de>
2026-03-03 20:39:27 +0100

    Minor change to CaffeineGame function signatures to be consistent with project style

--- include/caffeine-gl/game/CaffeineGame.hpp
+++ include/caffeine-gl/game/CaffeineGame.hpp
@@ -7,9 +7,9 @@ class CaffeineGame {
 public:
 	virtual ~CaffeineGame() = default;
 
-	virtual void Init() = 0;
-	virtual void Update(float deltaTime) = 0;
-	virtual void Render() = 0;
+	virtual void init() = 0;
+	virtual void update(float deltaTime) = 0;
+	virtual void render() = 0;
 };
 
 #endif //GAME_H
